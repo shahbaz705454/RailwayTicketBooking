9790fca4499b65c24a05cd8d80f4a1a40d313181
mohd shahbaz <81930428+shahbaz705454@users.noreply.github.com>
2025-10-25 21:20:46 +0530

    This is first Commit

---
+++ src/main/java/org/example/entities/User.java
@@ -0,0 +1,14 @@
+package org.example.entities;
+
+import java.util.List;
+
+public class User {
+    public String name;
+    private String password;
+    private  String hashedPassword;
+    private List<Ticket> ticketBooked;
+    private  String userId;
+
+
+
+}
---
+++ src/main/java/org/example/services/UserBookingService.java
@@ -0,0 +1,18 @@
+package org.example.services;
+
+import org.example.entities.User;
+
+public class UserBookingService {
+    private  User user;
+
+    public void checkTicket(){
+
+
+    }
+
+    public UserBookingService(User user) {
+
+
+
+    }
+}
