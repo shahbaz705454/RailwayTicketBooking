c411dd577f29b28bbd298f6a7e1d64f3bc426483
mohd shahbaz <81930428+shahbaz705454@users.noreply.github.com>
2025-10-26 16:47:20 +0530

    Modified

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,25 @@
 package org.example;
 
+import org.example.entities.User;
+import org.example.services.UserBookingService;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        User u2 = new User();
+
+
+        try
+        {
+            UserBookingService u1 = new UserBookingService(u2);
+
+            System.out.println(u1.getUserList());
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+
+
     }
 }
--- src/main/java/org/example/entities/User.java
+++ src/main/java/org/example/entities/User.java
@@ -9,6 +9,43 @@ public class User {
     private List<Ticket> ticketBooked;
     private  String userId;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    public List<Ticket> getTicketBooked() {
+        return ticketBooked;
+    }
+
+    public void setTicketBooked(List<Ticket> ticketBooked) {
+        this.ticketBooked = ticketBooked;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 }
